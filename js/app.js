efe4bc5aef84e0e714bc0a10956a589127627621
Raisa-Nuzhat <106836890+Raisa-Nuzhat@users.noreply.github.com>
2022-10-03 17:18:37 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,11 @@
+AOS.init();
+new VenoBox({
+    selector: ".popUpImage",
+    share: true,
+    spinner:'swing',
+});
+new VenoBox({
+    selector: ".play",
+    share: true,
+    spinner:'swing',
+});
